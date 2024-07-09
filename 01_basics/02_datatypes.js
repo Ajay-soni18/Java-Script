2441bf8509e092301e4d4cedb898db89bdd73992
Ajay-soni18 <ajaysoni7296@gmail.com>
2024-07-09 05:38:16 +0000

    learn about data types and converstion

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,10 @@
+"use strict"  // it will treat as all the code new version.
+
+//alert(3+3) it will work in browser console here we are using nodejs
+
+// data types =>
+// number , string , boolean ,null (object) , undefined
+let ajay;
+console.log(typeof ""); // typeof will tell us the datatype
+console.log(typeof null); // data type of null is object
+console.log(typeof ajay); // if varible is not intialised , datatype will be undefined
