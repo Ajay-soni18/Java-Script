89622726b5fad186f9d034d3d5a2f0f93ebf5a66
Ajay-soni18 <ajaysoni7296@gmail.com>
2024-07-12 16:21:57 +0000

    for in loop

---
+++ four.js
@@ -0,0 +1,26 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) { // for in loop for objects
+    //console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) { // for array key represents the index
+    //console.log(programming[key]);
+}
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) { // You can not itrate from for in loop in map
+//     console.log(key);
+// }
