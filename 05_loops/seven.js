d403b6aa6db55898151ad6a7a12f8770485f9425
Ajay-soni18 <ajaysoni7296@gmail.com>
2024-07-13 06:33:00 +0000

    chaining

--- 05_loops/seven.js
+++ 05_loops/seven.js
@@ -2,7 +2,7 @@ const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 // const newNums = myNumers.map( (num) => { return num + 10})
 
-const newNums = myNumers
+const newNums = myNumers // chainning
                 .map((num) => num * 10 )
                 .map( (num) => num + 1) // we put operation in the map
                 .filter( (num) => num >= 40) // we put condition in the filter 
